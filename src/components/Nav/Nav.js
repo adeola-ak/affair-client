3fd647c80c673cf9bc25d23672790044b9397676
adeola <adeola@Adeolas-MBP.fios-router.home>
2020-11-30 10:23:06 -0500

    fixed links

--- src/components/Nav/Nav.js
+++ src/components/Nav/Nav.js
@@ -1,56 +1,8 @@
-// import React from "react";
-import { Link } from "react-router-dom";
-// // import css
-// import "./Nav.css";
-
-// function Nav() {
-// 	const handleClickBars = () => {
-// 		let nav = document.getElementById("linksList");
-// 		if (nav.style.display === "flex") {
-// 			nav.style.display = "none";
-// 		} else {
-// 			nav.style.display = "flex";
-// 		}
-// 	};
-// 	const handleClick = () => {
-// 		let nav = document.getElementById("linksList");
-// 		nav.style.display = "none";
-// 	};
-// 	return (
-// 		<div className="navBar">
-// 			<header className="navBar">
-// 				<Link className="appTitle" to="/" onClick={handleClick}>
-// 					<div>affair</div>
-// 				</Link>
-
-// 				<div className="navLinks">
-// 					<Link to="/about" onClick={handleClick}>
-// 						<p className="link">about</p>
-// 					</Link>
-// 					<Link to="/contact" onClick={handleClick}>
-// 						<p className="link">contact</p>
-// 					</Link>
-// 				</div>
-
-// 				<div className="navIcon" onClick={handleClickBars}>
-// 					<i className="fas fa-bars"></i>
-// 				</div>
-// 			</header>
-// 			<nav id="linksList">
-// 				<Link className="navAbout" to="/About" onClick={handleClick}>
-// 					About
-// 				</Link>
-// 			</nav>
-// 		</div>
-// 	);
-// }
-
-// export default Nav;
-
 import React, { useState } from "react";
 import "./Nav.css";
 import MenuIcon from "@material-ui/icons/Menu";
 import MenuOpenIcon from "@material-ui/icons/MenuOpen";
+import { Link } from "react-router-dom";
 
 const Nav = () => {
 	const [click, setClick] = useState(false);
@@ -61,34 +13,40 @@ const Nav = () => {
 		<div className="header">
 			<div className="logo-nav">
 				<div className="logo-container">
-					<a href="/">affair</a>
+					<Link to="/">affair</Link>
 				</div>
 
 				<ul className={click ? "nav-options active" : "nav-options"}>
 					<li className="option" onClick={closeMobileMenu}>
-						<a href="/about">about</a>
+						<Link to="/about">about</Link>
 					</li>
 					<li className="option" onClick={closeMobileMenu}>
-						<a href="/contact">contact</a>
+						<Link to="/contact">contact</Link>
 					</li>
 					<li className="option" onClick={closeMobileMenu}>
-						<a href="blog">blog</a>
+						<Link to="blog">blog</Link>
 					</li>
 
 					<li
 						className="option mobile-option"
 						onClick={closeMobileMenu}
 					>
-						<a href="/login" className="sign-up">
+						{/* <a href="/login" className="sign-up">
 							SIGN-IN
-						</a>
+						</a> */}
+						<Link to="login" className="sign-up">
+							SIGN-IN
+						</Link>
 					</li>
 
 					<li
 						className="option mobile-option"
 						onClick={closeMobileMenu}
 					>
-						<a href="/new-user" className="sign-up">
+						{/* <a href="/new-user" className="sign-up">
+							SIGN-UP
+						</a> */}
+						<a href="/new-user" className="signup-btn">
 							SIGN-UP
 						</a>
 					</li>
@@ -96,12 +54,16 @@ const Nav = () => {
 			</div>
 			<ul className="signin-up">
 				<li className="sign-in" onClick={closeMobileMenu}>
-					<a href="/login">SIGN-IN</a>
+					<Link to="login">SIGN-IN</Link>
+					{/* <a href="/login">SIGN-IN</a> */}
 				</li>
 				<li onClick={closeMobileMenu}>
-					<a href="/new-user" className="signup-btn">
+					{/* <a href="/new-user" className="signup-btn">
 						SIGN-UP
-					</a>
+					</a> */}
+					<Link to="/new-user" className="signup-btn">
+						SIGN-UP
+					</Link>
 				</li>
 			</ul>
 
